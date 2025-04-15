acf7c1d39084c250133c88077afe5758ce647220
kiwi107 <119266066+kiwi107@users.noreply.github.com>
2025-04-15 13:18:39 +0200

    added cors, fixed auth middleware in server, and added routing in client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+
 
 function App() {
   return (
-    <div >
-
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
--- server/app.js
+++ server/app.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
 
 const app = express();
 const port = 8000;
 
-// Middleware to parse JSON bodies
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true, //allow cookies and auth headers to be sent
+}));
+app.use(cookieParser());
 app.use(express.json());
 
-
-// Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
